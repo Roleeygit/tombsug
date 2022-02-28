b49b4630ca8390b00f7c95015b75e17e354b33c5
Roleeygit <juhaszroli2001@gmail.com>
2022-02-28 08:52:20 +0100

    style javítás, javascript elkezdés

--- app.js
+++ app.js
@@ -7,3 +7,24 @@
 * Licenc: GNU GPL
 */
 
+var oldalIn = document.getElementById("oldal");
+var szogIn = document.getElementById("szog");
+var szamitgomb = document.getElementById("szamitgomb");
+var eredmenyIn = document.getElementById("eredmeny");
+
+szamitgomb.addEventListener("click", function() {
+  
+    if (anygaStr.match(minta)) {
+        console.log("OK")
+    } else {
+        console.log("Nem ok.")
+        alert("Hiba! Csak szám adható meg.")
+        return;
+    }
+
+    let anyag = Number(anyagIn.value);
+    console.log(anyag);
+    let reszecskek = anyag * 6 * Math.pow(10, 23);
+    console.log(reszecskek.toLocaleString());
+    eredmenyIn.value = reszecskek.toLocaleString();
+});
